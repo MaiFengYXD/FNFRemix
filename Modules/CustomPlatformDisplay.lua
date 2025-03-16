4be957699a1d5c6fd19836029da0479585565a7a
MaiFengYXD <maifengyx@gmail.com>
2025-03-16 16:07:36 +0800

    Update CustomPlatformDisplay.lua

--- Modules/CustomPlatformDisplay.lua
+++ Modules/CustomPlatformDisplay.lua
@@ -20,7 +20,7 @@ if game.PlaceId == 6520999642 then
         end
         if _G.FNFRemixACPD or getgenv and getgenv().FNFRemixACPD then
             StarterGui:SetCore('SendNotification', {
-                Title = 'Changed Context!',
+                Title = '🧐 Changed Context!',
                 Text = `Your display platform context has been changed to '{Context}', rejoin to see your changes.`,
                 Duration = 5
             })
@@ -43,7 +43,7 @@ if game.PlaceId == 6520999642 then
             if QueueOnTP then
                 task.spawn(function()
                     if ExecutorName == 'Xeno' or ExecutorName == 'Solara' then
-                        Message(`Your executor '{ExecutorName}' may not work properly.{'\n'}(Your executor will wait for the game to load, which will delay the execution time of queue_on_teleport)`)
+                        Message(`🤧 Your executor '{ExecutorName}' may not work properly.{'\n'}(Your executor will wait for the game to load, which will delay the execution time of queue_on_teleport)`)
                     end
                 end)
                 QueueOnTP([[
@@ -55,26 +55,26 @@ if game.PlaceId == 6520999642 then
                                 PreCheck = Speaker:WaitForChild'PlayerScripts'.ChildAdded:Connect(function(Child)
                                     if Child:IsA'LocalScript' and Child.Name == 'PlatformDisplay' then
                                         Child.Disabled = true
-                                        print'Disabled original display platform controller'
+                                        print'🤗 Disabled original display platform controller'
                                         PreCheck:Disconnect()
                                     end
                                 end)
                             end)
-                            print'Waiting for Remote Event'
+                            print'🥱 Waiting for Remote Event'
                             game:GetService'ReplicatedStorage':WaitForChild'Remotes':WaitForChild'PlatformRemoteEvent':FireServer(tostring(Context))
-                            print(`Fire Servered, your display platform was changed to '{Context}'`)
-                            warn"If the owner BitWork joins your server, you'd better leave the game quickly 😨"
+                            print(`😉 Fire Servered, your display platform was changed to '{Context}'`)
+                            warn"😨 If the owner BitWork joins your server, you'd better leave the game quickly"
                         else
                             local Message = Instance.new('Message', game:GetService'CoreGui')
-                            Message.Text = `Your executor '{ExecutorName}' does not support this script\n(missing isfile, readfile)`
+                            Message.Text = `😩 Your executor '{ExecutorName}' does not support this script\n(missing isfile, readfile)`
                             task.wait(5)
                             return Message:Destroy()
                         end
                     end
                 ]])
-                print(`Would you like to change your display platform context to '{Context}'?`)
+                print(`🤫 Would you like to change your display platform context to '{Context}'?`)
                 StarterGui:SetCore('SendNotification', {
-                    Title = 'Rejoin Required!',
+                    Title = '🧐 Rejoin Required!',
                     Text = 'Changing your display platform requires you to rejoin. Would you like to rejoin now?',
                     Button1 = 'Yes',
                     Button2 = 'No',
@@ -88,10 +88,10 @@ if game.PlaceId == 6520999642 then
                     _G.FNFRemixACPD = true
                 end
             else
-                Message(`Incompatible executor '{ExecutorName}'{'\n'}(missing queue_on_teleport)`)
+                Message(`😩 Incompatible executor '{ExecutorName}'{'\n'}(missing queue_on_teleport)`)
             end
         end
     else
-        return Message(`Your executor '{ExecutorName}' does not support this script{'\n'}(missing isfile, readfile, writefile)`)
+        return Message(`😩 Incompatible executor '{ExecutorName}'{'\n'}(missing isfile, readfile, writefile)`)
     end
 end
