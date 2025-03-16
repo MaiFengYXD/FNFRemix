15b5e3b756304557f6f527728f4303a6d0b1e9fe
MaiFengYXD <maifengyx@gmail.com>
2025-03-16 15:39:42 +0800

    Update CustomPlatformDisplay.lua

--- Modules/CustomPlatformDisplay.lua
+++ Modules/CustomPlatformDisplay.lua
@@ -71,7 +71,7 @@ if game.PlaceId == 6520999642 then
                     print'Waiting for Remote Event'
                     Cloneref(game:GetService'ReplicatedStorage'):WaitForChild'Remotes':WaitForChild'PlatformRemoteEvent':FireServer(tostring(Context))
                     print(`Fire Servered, your display platform was changed to '{Context}'`)
-                    print'If owner BitWork joined your server, you better leave the game 😨'
+                    print'If the owner BitWork joins your server, you'd better leave the game 😨'
                 end
             ]])
             StarterGui:SetCore('SendNotification', {
